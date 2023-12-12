7945b9192ba48b54268e90ef16d86945444035a2
EliasPiolatto <eliaspiolatto77@hotmail.com>
2023-12-12 15:29:39 -0300

    funciona, pero faltan modificaciones personales

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showNav, setShowNav] = useState(false);
+
+  const openNav = () => {
+    setShowNav(true);
+  };
+
+  const closeNav = () => {
+    setShowNav(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={`container ${showNav ? 'show-nav' : ''}`}>
+        <div className="circle-container">
+          <div className="circle">
+            <button id="close" onClick={closeNav}>
+              <i className="fas fa-times"></i>
+            </button>
+            <button id="open" onClick={openNav}>
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+        </div>
+
+        <div className="content">
+          <h1>Amazing Article</h1>
+          <small>Florin Pop</small>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia in ratione dolores cupiditate, maxime aliquid impedit dolorem nam dolor omnis atque fuga labore modi veritatis porro laborum minus, illo, maiores recusandae cumque ipsa quos. Tenetur, consequuntur mollitia labore pariatur sunt quia harum aut. Eum maxime dolorem provident natus veritatis molestiae cumque quod voluptates ab non, tempore cupiditate? Voluptatem, molestias culpa. Corrupti, laudantium iure aliquam rerum sint nam quas dolor dignissimos in error placeat quae temporibus minus optio eum soluta cupiditate! Cupiditate saepe voluptates laudantium. Ducimus consequuntur perferendis consequatur nobis exercitationem molestias fugiat commodi omnis. Asperiores quia tenetur nemo ipsa.
+          </p>
+
+          <h3>My Dog</h3>
+          <img src="https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" alt="doggy" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit libero deleniti rerum quo, incidunt vel consequatur culpa ullam. Magnam facere earum unde harum. Ea culpa veritatis magnam at aliquid. Perferendis totam placeat molestias illo laudantium? Minus id minima doloribus dolorum fugit deserunt qui vero voluptas, ut quia cum amet temporibus veniam ad ea ab perspiciatis, enim accusamus asperiores explicabo provident. Voluptates sint, neque fuga cum illum, tempore autem maxime similique laborum odio, magnam esse. Aperiam?
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <nav>
+        <ul>
+          <li><i className="fas fa-home"></i><a href="#"> Home</a></li>
+          <li><i className="fas fa-user-alt"></i><a href="#"> About</a></li>
+          <li><i className="fas fa-envelope"></i><a href="#"> Contact</a></li>
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
